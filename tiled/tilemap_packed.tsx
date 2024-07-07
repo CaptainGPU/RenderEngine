7bad5ee95c779e3171a2d9bbb86ab983e7691878
CaptainGPU <377971+CaptainGPU@users.noreply.github.com>
2024-07-07 22:24:34 +0300

    Add Tiled projects

---
+++ tiled/tilemap_packed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tilemap_packed" tilewidth="18" tileheight="18" tilecount="180" columns="20">
+ <image source="tilemap_packed.png" width="360" height="162"/>
+</tileset>
