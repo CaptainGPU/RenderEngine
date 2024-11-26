0772f8121f3277d0615900de32d37d1bab4a2c39
CaptainGPU <377971+CaptainGPU@users.noreply.github.com>
2024-11-26 15:02:58 +0200

    Update tilemaps

--- resources/TiledMaps/test_level_tilemap.tsx
+++ resources/TiledMaps/test_level_tilemap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="test_level_tilemap" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../textures/test_level_tilemap.png" width="512" height="512"/>
+<tileset version="1.10" tiledversion="1.11.0" name="test_level_tilemap" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="../../textures/test_level_tilemap.png" width="1024" height="1024"/>
 </tileset>
